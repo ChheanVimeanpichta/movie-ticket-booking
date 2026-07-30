be5b53025f6df4c7ba8be35d39263872bef5d0e5
Yann sreyvin <yannsreyvin@gmail.com>
2026-07-30 22:25:25 +0700

    Add claim offer page and update offers booking layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,15 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "@/pages/Home";
 import ComingSoon from "@/pages/ComingSoon";
-
+import Offers from "@/pages/Offers";
+import ClaimOffer from "./pages/ClaimOffer";
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/coming-soon" element={<ComingSoon />} />
+      <Route path="/offers" element={<Offers />} />
+      <Route path="/claim-offer" element={<ClaimOffer />} />
     </Routes>
   );
 }
